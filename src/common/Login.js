81328c07d16f85ed06a646ea5920ba17d05043cc
Naveen Gupta <naveen.gpta288@gmail.com>
2021-06-29 02:35:05 +0530

    added login and  registration in modal component  with complete header section

---
+++ src/common/Login.js
@@ -0,0 +1,97 @@
+import React,{useState} from 'react'
+import { Input,InputLabel,Button } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
+import { useHistory } from 'react-router';
+import './Login.css'
+import { makeStyles } from '@material-ui/styles';
+
+
+const useStyles=makeStyles(theme=>({
+    formControl:{
+        margin:theme.spacing,
+        minWidth:240,
+        maxWidth:240,
+    },
+
+    chip:{
+        margin: theme.spacing/4
+    }
+}))
+
+
+export default function Login() {
+
+
+    const classes=useStyles();
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState('');
+    const[isLoggedIn, setLoggedIn]=useState(false);
+    const history=useHistory();
+
+    const login=async()=>{
+        console.log(email,password)
+
+
+
+        let item={email,password};
+        const params=window.btoa(`${email}:${password}`)
+
+        console.log(params);
+
+        let result=await fetch('/api/v1/auth/login',{
+            method:'POST',
+            headers:{
+                "content-type":"application/json",
+                "Accept": "application/json;charset=UTF-8",
+                 "authorization": `Basic ${params}`
+            },
+
+            body:JSON.stringify(item)
+        });
+
+        result=await result.json();
+        localStorage.setItem("user-info",JSON.stringify(result))
+        setLoggedIn(true)
+       // history.push('/');
+
+
+    }
+    return (
+        <div className='login-form'>
+            <FormControl required className={classes.FormControl}>
+                <InputLabel htmlFor='email'>Username</InputLabel>
+                <Input
+                required
+                id='email'
+                name='email'
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+            </FormControl>
+            <br/>
+            <br/>
+
+            <FormControl required className={classes.FormControl}>
+                <InputLabel htmlFor='password'>Password</InputLabel>
+                <Input
+                required
+                id='password'
+                name='password'
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                />
+            </FormControl>
+            <br/>
+            <br/>
+            <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            onClick={login}
+            >
+             Login
+            </Button>
+            
+        </div>
+    )
+}
--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,8 +1,11 @@
 import React  from "react";
+
+//import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from '../common/header/Header'
+
 export default function Controller(params) {
     return (<div>
-       <h1>Movie App</h1>
-
+           <Header></Header>
 
     </div>);
 }
